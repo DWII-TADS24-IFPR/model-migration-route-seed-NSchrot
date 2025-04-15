2020319d9611c62eef62a8a3529c1b2ce39c6ead
Nathan <nathanhenrique64@gmail.com>
2025-04-15 19:39:36 -0300

    Recriado Migrações

--- migrations/app/Models/Aluno.php
+++ migrations/app/Models/Aluno.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Aluno extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'alunos';
+
+    protected $fillable = [
+        'nome',
+        'cpf',
+        'email',
+        'senha',
+        'user_id',
+        'curso_id',
+        'turma_id',
+    ];
 }
